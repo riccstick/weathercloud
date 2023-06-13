a95137dd66e1dbc3f9a56e486dba87129e4762bf
erik_breslmayr <erik.breslmayr@werum.com>
2023-06-13 19:04:25 +0200

    upate push to domain

--- upload2domain.sh
+++ upload2domain.sh
@@ -1,5 +1,7 @@
 #! /usr/bin/bash
 
-curl -T 'Regen (mm).png' -u prandstritzko.:J0cJ*vY2f ftp://www.prand-stritzko.at/httpdocs/wp-content/uploads/weather/
+DIR="/home/ricci/weathercloud"
 
-curl -T 'Temperatur (°C).png' -u prandstritzko.:J0cJ*vY2f ftp://www.prand-stritzko.at/httpdocs/wp-content/uploads/weather/
+curl -T $DIR/'Regen (mm).png' -u prandstritzko.:J0cJ*vY2f ftp://www.prand-stritzko.at/httpdocs/wp-content/uploads/weather/
+
+curl -T $DIR/'Temperatur (°C).png' -u prandstritzko.:J0cJ*vY2f ftp://www.prand-stritzko.at/httpdocs/wp-content/uploads/weather/
